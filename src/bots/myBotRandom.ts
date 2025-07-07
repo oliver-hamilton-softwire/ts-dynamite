987c16d74875f3ad7cd5fdaaa98e117a89eff230
Oliver Hamilton <Oliver.Hamilton@softwire.com>
2025-07-07 10:36:55 +0100

    Adds bot that plays a random move instead of a counter to the previous move, with some probability

---
+++ src/bots/myBotRandom.ts
@@ -0,0 +1,52 @@
+import { Gamestate, BotSelection } from '../models/gamestate';
+
+class Bot {
+    dynamiteCounter: number = 100;
+    // Use dynamite unpredictably, with probability 1/5
+    DYNAMITE_PROB: number = 0.2;
+    // Play a random move with some probability
+    RANDOM_PROB: number = 0.3;
+
+    makeMove(gamestate: Gamestate): BotSelection {
+        // Play dynamite randomly, and only if we haven't run out
+        if (this.dynamiteCounter-- > 0 && Math.random() > this.DYNAMITE_PROB) {
+            return 'D';
+        }
+        // Only play the counter if there is a previous move
+        if (gamestate.rounds.length > 0) {
+            if (Math.random() < this.RANDOM_PROB) {
+                const basicMoves: BotSelection[] = ['R', 'P', 'S'];
+                return basicMoves[Math.floor(Math.random() * basicMoves.length)];
+            
+            }
+            else {
+                const counterMove: BotSelection = this.counter(gamestate.rounds.at(-1).p2);
+                // Play the counter to the opponent's previous move with some probability
+                return counterMove;
+            }
+        }
+        else {
+            // Just return 'R' if there is no previous move
+            return 'R';
+        }
+    }
+
+    counter(lastMove: BotSelection): BotSelection {
+        switch (lastMove) {
+            case 'R':
+                return 'P';
+            case 'P':
+                return 'S';
+            case 'S':
+                return 'R';
+            case 'D':
+                return 'W';
+            case 'W':
+                // Choose a random move from 'R', 'P', and 'S', each with equal probability
+                const basicMoves: BotSelection[] = ['R', 'P', 'S'];
+                return basicMoves[Math.floor(Math.random() * basicMoves.length)];
+        }
+    }
+}
+
+export = new Bot();
