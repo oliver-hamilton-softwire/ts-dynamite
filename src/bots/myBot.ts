52f25fd08b18d969a363077e7a5bda4c60162876
Oliver Hamilton <Oliver.Hamilton@softwire.com>
2025-07-07 10:22:40 +0100

    Use dynamite and water bombs, and counter the opponents last move

--- src/bots/myBot.ts
+++ src/bots/myBot.ts
@@ -1,10 +1,33 @@
 import { Gamestate, BotSelection } from '../models/gamestate';
 
 class Bot {
+    dynamiteCounter: number = 100;
+
     makeMove(gamestate: Gamestate): BotSelection {
+        // Use dynamite unpredictably, with probability 1/5
+        if (this.dynamiteCounter-- > 0 && Math.random() > 0.2) {
+            return 'D';
+        }
         const allMoves: BotSelection[] = ['R', 'P', 'S'];
-        // Choose a random move, with equal probability for each
-        return allMoves[Math.floor(Math.random() * allMoves.length)];
+        const counterMove: BotSelection = this.counter(gamestate.rounds.at(-1).p2);
+        // Play the counter to the opponent's previous move
+        return counterMove;
+    }
+
+    counter(lastMove: BotSelection): BotSelection {
+        switch (lastMove) {
+            case 'R':
+                return 'P';
+            case 'P':
+                return 'S';
+            case 'S':
+                return 'R';
+            case 'D':
+                return 'W';
+            case 'W':
+                const basicMoves: BotSelection[] = ['R', 'P', 'S'];
+                return basicMoves[Math.floor(Math.random() * basicMoves.length)];
+        }
     }
 }
 
