1c324783fdf693c027ce58ee55c24dd80cf47dab
Oliver Hamilton <Oliver.Hamilton@softwire.com>
2025-07-07 11:32:29 +0100

    Adds a bot that tries to outsmart a bot that counters previous moves

---
+++ src/bots/myBotCounter.ts
@@ -0,0 +1,64 @@
+import { Gamestate, BotSelection } from '../models/gamestate';
+
+class Bot {
+    dynamiteCounter: number = 100;
+    // Use dynamite unpredictably, with probability 1/2
+    DYNAMITE_PROB: number = 0.5;
+    // Play a random move with some probability
+    RANDOM_PROB: number = 1/3;
+    // Play the counter to what the opponent would play if they're trying to counter my last move
+    COUNTER_COUNTER_PROB: number = 1/3;
+
+    makeMove(gamestate: Gamestate): BotSelection {
+        // Play dynamite randomly, and only if we haven't run out
+        if (this.dynamiteCounter-- > 0 && Math.random() > this.DYNAMITE_PROB) {
+            return 'D';
+        }
+        // Only play the counter if there is a previous move
+        if (gamestate.rounds.length > 0) {
+            /* Randomise between:
+                - A random move
+                - The counter to the opponent's last move
+                - The counter to what the opponent would play if they're trying to counter my last move
+            */
+            if (Math.random() < this.RANDOM_PROB) {
+                const basicMoves: BotSelection[] = ['R', 'P', 'S'];
+                return basicMoves[Math.floor(Math.random() * basicMoves.length)];
+            }
+            else if (Math.random() < this.RANDOM_PROB + this.COUNTER_COUNTER_PROB) {
+                const counterMove: BotSelection = this.counter(this.counter(gamestate.rounds.at(-1).p1));
+
+                return counterMove;
+            
+            }
+            else {
+                const counterMove: BotSelection = this.counter(gamestate.rounds.at(-1).p2);
+                // Play the counter to the opponent's previous move with some probability
+                return counterMove;
+            }
+        }
+        else {
+            // Just return 'R' if there is no previous move
+            return 'R';
+        }
+    }
+
+    counter(lastMove: BotSelection): BotSelection {
+        switch (lastMove) {
+            case 'R':
+                return 'P';
+            case 'P':
+                return 'S';
+            case 'S':
+                return 'R';
+            case 'D':
+                return 'W';
+            case 'W':
+                // Choose a random move from 'R', 'P', 'S', and 'W', each with equal probability
+                const basicMoves: BotSelection[] = ['R', 'P', 'S', 'W'];
+                return basicMoves[Math.floor(Math.random() * basicMoves.length)];
+        }
+    }
+}
+
+export = new Bot();
